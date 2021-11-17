bc48c487a9034ba094cb1b5cd3601c591af1762e
Mike <mikekett4@yahoo.com>
2021-11-17 08:56:29 -0700

    Patch function has error

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,9 +3,9 @@ import Search from './Search';
 
 
 
-function MainContainer ({props, search, setSearch}) {
+function MainContainer ({props, search, setSearch, handleUpdateLike}) {
 
-    const artist = props.map((propsObj) => <AlbumCards id={propsObj.id} name={propsObj.name} genre={propsObj.genre} bio={propsObj.bio} image={propsObj.image} liked={propsObj.liked} youtube={propsObj.youtube} key={propsObj.id}/>)
+    const artist = props.map((propsObj) => <AlbumCards id={propsObj.id} name={propsObj.name} genre={propsObj.genre} bio={propsObj.bio} image={propsObj.image} liked={propsObj.liked} youtube={propsObj.youtube} key={propsObj.id} handleUpdateLike={handleUpdateLike}/>)
 
     return (
         <div>
